11d1fcd163936be337fe79abca0a0163f179f49a
kinyuaaa-code <thebestelvis3@gmail.com>
2026-07-30 09:51:26 +0300

    Add service worker registration for push notifications

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,23 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Register Firebase messaging service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered:', registration.scope);
+    })
+    .catch((err) => {
+      console.error('Service Worker registration failed:', err);
+    });
+}
+
 reportWebVitals();
